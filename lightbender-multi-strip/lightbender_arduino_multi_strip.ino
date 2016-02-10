c10e337b9de550b587b0be9f6eef901a3d89cf7b
Eric Busch <eric@atdetroit.com>
2016-02-10 12:09:51 -0500

    added multi strip support for lightbender
    Signed-off-by: Eric Busch <eric@atdetroit.com>

--- lightbender-multi-strip/lightbender_arduino_multi_strip.ino
+++ lightbender-multi-strip/lightbender_arduino_multi_strip.ino
@@ -4,7 +4,7 @@
 #endif
 
 #define PIN1 3
-#define PIN2 4
+#define PIN2 5
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
@@ -13,26 +13,29 @@
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(150, PIN1, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(150, PIN1, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(60, PIN2, NEO_GRB + NEO_KHZ800);
+
 void setup() {
   pinMode(6, OUTPUT);
   pinMode(7, INPUT);
   digitalWrite(7, HIGH);
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
+  strip1.begin();
+  strip2.begin();
+  strip1.show(); // Initialize all pixels to 'off'
+  strip2.show(); // Initialize all pixels to 'off'
 }
 
 void loop() {
 
-  
-  if (digitalRead(7)) {
-
-    fadeInFadeOutWheel(10, 5000, 255, 81);
-  } 
+    //fadeInFadeOutWheel(strip1, 10, 5000, 255, 81);
+    //fadeInFadeOutWheel(strip2, 10, 5000, 255, 81);
+    rainbow(strip1, 0);
+    rainbow(strip2, 0);
 }
 
 // Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
+void colorWipe(Adafruit_NeoPixel &strip, uint32_t c, uint8_t wait) {
   for(uint16_t i=0; i<strip.numPixels(); i++) {
     strip.setPixelColor(i, c);
     strip.show();
@@ -40,12 +43,12 @@ void colorWipe(uint32_t c, uint8_t wait) {
   }
 }
 
-void rainbow(uint8_t wait) {
+void rainbow(Adafruit_NeoPixel &strip, uint8_t wait) {
   uint16_t i, j;
 
   for(j=0; j<256; j++) {
     for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
+      strip.setPixelColor(i, Wheel(strip, (i+j) & 255));
     }
     strip.show();
     delay(wait);
@@ -53,12 +56,12 @@ void rainbow(uint8_t wait) {
 }
 
 // Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
+void rainbowCycle(Adafruit_NeoPixel &strip, uint8_t wait) {
   uint16_t i, j;
 
   for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
     for(i=0; i< strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+      strip.setPixelColor(i, Wheel(strip, ((i * 256 / strip.numPixels()) + j) & 255));
     }
     strip.show();
     delay(wait);
@@ -66,7 +69,7 @@ void rainbowCycle(uint8_t wait) {
 }
 
 //Theatre-style crawling lights.
-void theaterChase(uint32_t c, uint8_t wait) {
+void theaterChase(Adafruit_NeoPixel &strip, uint32_t c, uint8_t wait) {
   for (int j=0; j<10; j++) {  //do 10 cycles of chasing
     for (int q=0; q < 3; q++) {
       for (int i=0; i < strip.numPixels(); i=i+3) {
@@ -84,11 +87,11 @@ void theaterChase(uint32_t c, uint8_t wait) {
 }
 
 //Theatre-style crawling lights with rainbow effect
-void theaterChaseRainbow(uint8_t wait) {
+void theaterChaseRainbow(Adafruit_NeoPixel &strip, uint8_t wait) {
   for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
     for (int q=0; q < 3; q++) {
       for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+        strip.setPixelColor(i+q, Wheel(strip, (i+j) % 255));    //turn every third pixel on
       }
       strip.show();
 
@@ -103,7 +106,7 @@ void theaterChaseRainbow(uint8_t wait) {
 
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
+uint32_t Wheel(Adafruit_NeoPixel &strip, byte WheelPos) {
   WheelPos = 255 - WheelPos;
   if(WheelPos < 85) {
     return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
@@ -117,7 +120,7 @@ uint32_t Wheel(byte WheelPos) {
 }
 
 
-uint32_t intensityWheel(byte WheelPos, double intensity) {
+uint32_t intensityWheel(Adafruit_NeoPixel &strip, byte WheelPos, double intensity) {
   WheelPos = 255 - WheelPos;
   if(WheelPos < 85) {
     return strip.Color((255 - WheelPos * 3) * intensity, 0, (WheelPos * 3) * intensity);
@@ -131,7 +134,7 @@ uint32_t intensityWheel(byte WheelPos, double intensity) {
 }
 
 
-void fadeInFadeOut(int speed, int holdTime, int r, int g, int b) {
+void fadeInFadeOut(Adafruit_NeoPixel &strip, int speed, int holdTime, int r, int g, int b) {
 
   float intensity = 0;
   uint32_t color;
@@ -161,7 +164,7 @@ void fadeInFadeOut(int speed, int holdTime, int r, int g, int b) {
   }
 }
 
-void fadeInFadeOutWheel(int speed, int holdTime, int startColor, int endColor) {
+void fadeInFadeOutWheel(Adafruit_NeoPixel &strip, int speed, int holdTime, int startColor, int endColor) {
 
   double intensity = 0;
   int colorDifference = endColor - startColor;
@@ -172,7 +175,7 @@ void fadeInFadeOutWheel(int speed, int holdTime, int startColor, int endColor) {
 
     wheelPos += increment;
     intensity += 0.01;
-    color = intensityWheel((uint8_t)wheelPos, intensity);
+    color = intensityWheel(strip, (uint8_t)wheelPos, intensity);
     for(int j=0; j<strip.numPixels(); j++) {
 
       strip.setPixelColor(j, color);
@@ -187,7 +190,7 @@ void fadeInFadeOutWheel(int speed, int holdTime, int startColor, int endColor) {
 
     wheelPos -= increment;
     intensity -= 0.01;
-    color = intensityWheel((uint8_t)wheelPos, intensity);
+    color = intensityWheel(strip, (uint8_t)wheelPos, intensity);
     for(int j=0; j<strip.numPixels(); j++) {
 
       strip.setPixelColor(j, color);
