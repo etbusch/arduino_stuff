5b79f5a8a2163be823bfedf2b2bf6a4528b7e62a
Eric Busch <eric@atdetroit.com>
2016-02-17 14:36:15 -0500

    async lightbender has been stubbed out to minimal function
    Signed-off-by: Eric Busch <eric@atdetroit.com>

--- lightbender_async/Strip.h
+++ lightbender_async/Strip.h
@@ -1,4 +1,6 @@
 #include <Adafruit_NeoPixel.h>
+#define HANG_TIME 5000
+#define DISABLED_TIME 15000
 
 enum StripRoutine {
   fade,
@@ -12,6 +14,8 @@ class Strip {
     Adafruit_NeoPixel strip;
     StripRoutine routine;
     int direction;
+    int waitUntil;
+    double fadeSteps;
 
     void setStripColor(uint32_t color) {
       
@@ -41,7 +45,7 @@ class Strip {
     }
 
 
-    uint32_t intensitywheel(byte wheelPos, double intensity) {
+    uint32_t intensityWheel(byte wheelPos, double intensity) {
       
       wheelPos = 255 - wheelPos;
       if(wheelPos < 85) {
@@ -57,19 +61,58 @@ class Strip {
       return strip.Color((wheelPos * 3) * intensity, (255 - wheelPos * 3) * intensity, 0);
     }
 
+    void fadeStep() {
+
+      if (waiting) {
+
+        if (millis() >= waitUntil) {
+
+          waiting = 0;
+          direction = -1;
+        }
+      } else {
+        int stepNum;
+
+        if (direction == 1) {
+
+          stepNum = fadeSteps - stepsLeft;
+        } else {
+
+          stepNum = stepsLeft;
+        }
+        double intensity = stepNum / fadeSteps;
+        this->setStripColor(this->color(fadeColor[0], fadeColor[1], fadeColor[2], intensity));
+        stepsLeft--;
+        if (stepsLeft == 0 && direction == 1) {
+
+          waiting = 1;
+          waitUntil = millis() + HANG_TIME;
+        } else if (stepsLeft == 0 && direction == -1) {
+
+          active = 0;
+        }
+      }
+    }
 
   public:
 
     bool active;
-    bool changing;
-    int stepsLeft;  
+    bool waiting;
+    int stepsLeft;
+    uint32_t fadeColor[3];
   
     Strip(uint16_t ledCount = 300, uint8_t pin = 1) {
       
       strip = Adafruit_NeoPixel(ledCount, pin, NEO_GRB + NEO_KHZ800);
+      strip.begin();
+      strip.show();
       active = 0;
-      changing = 0;
+      waiting = 0;
       direction = 1;
+      fadeSteps = 25;
+      fadeColor[0] = 255;
+      fadeColor[1] = 255;
+      fadeColor[2] = 255;
     }
 
     void startRoutine(StripRoutine newRoutine) {
@@ -77,7 +120,7 @@ class Strip {
       routine = newRoutine;
       active = 1;
       direction = 1;
-      stepsLeft = FADE_STEPS;
+      stepsLeft = fadeSteps;
     }
 
     void step() {
@@ -87,7 +130,7 @@ class Strip {
         switch (routine) {
 
           case fade:
-            
+            this->fadeStep();
             break;
 
           case intensityFade:
@@ -100,4 +143,5 @@ class Strip {
       
       return strip.Color(r * intensity, g * intensity, b * intensity);
     }
+
 };
